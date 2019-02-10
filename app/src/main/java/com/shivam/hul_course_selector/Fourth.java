c4634f384d494206491608be0a86f8ac414697c4
Flash-Shivam <34097420+Flash-Shivam@users.noreply.github.com>
2019-02-11 01:50:04 +0530

    Commit54 :  Storing in SQLDatabase

--- app/src/main/java/com/shivam/hul_course_selector/Fourth.java
+++ app/src/main/java/com/shivam/hul_course_selector/Fourth.java
@@ -9,13 +9,14 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.w3c.dom.Text;
 
 public class Fourth extends Activity {
 
 
-    SQLiteDatabase db;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,84 +30,17 @@ public class Fourth extends Activity {
 
         TextView v = (TextView) findViewById(R.id.tr);
 
-        db = openOrCreateDatabase("My Database", MODE_PRIVATE, null);
-
-        db.execSQL("Create table if not exists Mytable (a String,b String);");
-
-
-         String a = "", b = "", c = "", d = "";
-
-        int e = 0;
-
-        for (int i = 0; i < p.length(); i++) {
-            if (p.charAt(i) == '@') {
-                e = 1;
-            }
-
-            if (p.charAt(i) == '%') {
-                e = 2;
-            }
-
-            if (p.charAt(i) == '#') {
-                e = 3;
-            }
-
-            if (e == 0) {
-                a = a + p.charAt(i);
-            } else if (e == 1) {
-                if (p.charAt(i) != '@')
-                    b = b + p.charAt(i);
-            } else if (e == 2) {
-                if (p.charAt(i) != '%')
-                    c = c + p.charAt(i);
-            } else if (e == 3) {
-                if (p.charAt(i) != '#')
-                    d = d + p.charAt(i);
-            }
-        }
-            final String aa = a,bb=b;
         Button f = (Button) findViewById(R.id.but2);
         Button g = (Button) findViewById(R.id.but);
-        f.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Sr();
-            }
-        });
 
-        g.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+        v.setText(p);
 
-                Wr(aa,bb);
-            }
-        });
 
     }
 
-    public void Wr(String a,String b){
 
-        db.execSQL("Insert into MyTable values (" + a + "," + b + ");");
-    }
 
-    public void Sr(){
-        Cursor cq;
-        String temp;
-        String Data = "";
 
-        cq = db.rawQuery("Select * from MyTable;", null);
-
-        cq.moveToFirst();
-
-        for (int i = 0; cq.moveToPosition(i); i++) {
-            temp = cq.getString(0);
-            Data = Data + temp;
-            temp = cq.getString(1);
-            Data = Data + temp;
-        }
-        TextView qw = (TextView) findViewById(R.id.tr);
-        qw.setText(Data);
-    }
 
 }
 
--- app/src/main/java/com/shivam/hul_course_selector/ThirdScreen.java
+++ app/src/main/java/com/shivam/hul_course_selector/ThirdScreen.java
@@ -2,12 +2,20 @@ package com.shivam.hul_course_selector;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Random;
 
 public class ThirdScreen extends Activity {
+
+    SQLiteDatabase db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,8 +29,42 @@ public class ThirdScreen extends Activity {
 
         TextView v = (TextView) findViewById(R.id.ter1);
 
+        db = openOrCreateDatabase("My Database", MODE_PRIVATE, null);
+
+        db.execSQL("Create table if not exists Mytable (Roll int,Marks int);");
+
+        Button tr = (Button) findViewById(R.id.Register);
+
+        tr.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Wr();
+            }
+        });
+
+        Button f = (Button) findViewById(R.id.View) ;
+        f.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+                Sr();
+
+            }
+        });
+
     }
 
+
+    public void Wr(){
+
+        int roll,marks;
+        Random r = new Random();
+        roll=r.nextInt(40);
+        marks=r.nextInt(100);
+        db.execSQL("Insert into MyTable values (" + Integer.toString(roll) + ", " + Integer.toString(marks) + ");");
+
+    }
 
     public void Cha(View view) {
 
@@ -32,8 +74,28 @@ public class ThirdScreen extends Activity {
         EditText s = (EditText) findViewById(R.id.yar2);
         EditText t = (EditText) findViewById(R.id.yar3);
         EditText v = (EditText) findViewById(R.id.yar4);
-        chanescreen.putExtra("callA",r+"@"+s+"%"+t+"#"+v);
+        chanescreen.putExtra("callA",r.getText().toString()+" "+s.getText().toString()+" "+t.getText().toString()+" "+v.getText().toString());
 
         startActivity(chanescreen);
+    }
+
+
+    public void Sr(){
+        Cursor cq;
+        int temp;
+        String Data = "";
+
+        cq = db.rawQuery("Select * from MyTable;", null);
+
+        cq.moveToFirst();
+
+        for (int i = 0; cq.moveToPosition(i); i++) {
+            temp = cq.getInt(0);
+            Data = Data + Integer.toString(temp);
+            temp = cq.getInt(1);
+            Data = Data + Integer.toString(temp);
+        }
+        TextView qw = (TextView) findViewById(R.id.feedback);
+        qw.setText(Data);
     }
 }
