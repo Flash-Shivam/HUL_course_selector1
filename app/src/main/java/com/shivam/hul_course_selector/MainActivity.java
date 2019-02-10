1e1ccea953579f2dbf1ce1b956dc654d7d94475a
Flash-Shivam <34097420+Flash-Shivam@users.noreply.github.com>
2019-02-10 22:03:36 +0530

    first commit

---
+++ app/src/main/java/com/shivam/hul_course_selector/MainActivity.java
@@ -0,0 +1,186 @@
+package com.shivam.hul_course_selector;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+import java.lang.Float;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String[] Course = new String[]{
+            "HUL212","HUL213","HUL232","HUL239"
+    };
+
+    RadioGroup rg ;
+    RadioButton rb ;
+    TextView tv;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+         rg  =  findViewById(R.id.Radio_group);
+
+
+
+        Button z = findViewById(R.id.gn);
+
+        z.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                AutoCompleteTextView ed = findViewById(R.id.Auto);
+                TextView x = findViewById(R.id.cn);
+                String q= ed.getText().toString();
+                if(q.equals("HUL212"))
+                {
+                    x.setText("Microeconomics");
+                }
+                else  if(q.equals("HUL213"))
+                {
+                    x.setText("Macroeconomics");
+                }
+                else if(q.equals("HUL232"))
+                {
+                    x.setText("Modern Indian Fiction in Translation");
+                }
+                else if(q.equals("HUL239"))
+                {
+                    x.setText("Indian Fiction in English");
+                }
+
+            }
+        });
+
+        AutoCompleteTextView ed = findViewById(R.id.Auto);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1 ,Course   );
+        ed.setAdapter(adapter);
+
+    }
+
+    public void checkButton(View v)
+    {
+        int radioId = rg.getCheckedRadioButtonId();
+
+        rb = findViewById(radioId);
+
+        Toast.makeText(this, "Selected Button: " + rb.getText(), Toast.LENGTH_SHORT).show();
+    }
+
+
+
+    public void Fetch_Result(View view) {
+
+        AutoCompleteTextView ed = findViewById(R.id.Auto);
+        String u= ed.getText().toString();
+        EditText Cg = (EditText) findViewById(R.id.editText2);
+        if(!isEmpty(Cg))
+        {
+
+
+
+
+
+                String f = Cg.getText().toString();
+                int j = 0,k=0;
+                for(int i=0;i<f.length();i++)
+                {
+                    if(f.charAt(i)>= 48&&f.charAt(i)<=57)
+                    {
+                        j++;
+                    }
+                    if((f.charAt(i)==46))
+                    {
+                        k++;
+                    }
+                }
+                if((j==f.length()&&k==0)||(j==f.length()-1&&k==1)) {
+                    float q = Float.valueOf(f);
+                    if(u.equals("HUL212")){
+                            double y = 7.5;
+
+                        if (Double.compare(q, y) < 0) {
+                            Toast.makeText(this, "Not Likely to get this course due CG<"+Double.toString(y), Toast.LENGTH_LONG).show();
+                        } else {
+                            Toast.makeText(this, "Click on Course Details", Toast.LENGTH_LONG).show();
+                        }
+                    }
+                    else if(u.equals("HUL213")){
+                        double y = 7.5;
+
+                        if (Double.compare(q, y) < 0) {
+                            Toast.makeText(this, "Not Likely to get this course due CG<"+Double.toString(y), Toast.LENGTH_LONG).show();
+                        } else {
+                            Toast.makeText(this, "Click on Course Details", Toast.LENGTH_LONG).show();
+                        }
+                    }
+                    else if(u.equals("HUL232")){
+                        double y = 8;
+
+                        if (Double.compare(q, y) < 0) {
+                            Toast.makeText(this, "Not Likely to get this course due CG<"+Double.toString(y), Toast.LENGTH_LONG).show();
+                        } else {
+                            Toast.makeText(this, "Click on Course Details", Toast.LENGTH_LONG).show();
+                        }
+                    }
+                    else if(u.equals("HUL239")){
+                        double y = 0;
+
+                        if (Double.compare(q, y) < 0) {
+                            Toast.makeText(this, "Not Likely to get this course due CG<"+Double.toString(y), Toast.LENGTH_LONG).show();
+                        } else {
+                            Toast.makeText(this, "Click on Course Details", Toast.LENGTH_LONG).show();
+                        }
+                    }
+
+
+                }
+                else{
+                    Toast.makeText(this, "Enter Valid CG", Toast.LENGTH_LONG).show();
+                }
+
+        }
+        else{
+            Toast.makeText(this, "Enter Valid CG", Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+    protected boolean isEmpty(EditText e)
+    {
+        return e.getText().toString().trim().length()==0;
+    }
+
+    public void switchp(View view) {
+
+        Intent changescreen = new Intent(this,SecondScreen.class);
+
+
+
+        AutoCompleteTextView ed = findViewById(R.id.Auto);
+        String q= ed.getText().toString();
+        RadioButton rb;
+        RadioGroup rg;
+        rg =  findViewById(R.id.Radio_group);
+        int radioId = rg.getCheckedRadioButtonId();
+        rb = findViewById(radioId);
+        String p = rb.getText().toString();
+        changescreen.putExtra("callAc",q+" "+p);
+
+        startActivity(changescreen);
+    }
+}
---
+++ app/src/main/java/com/shivam/hul_course_selector/SecondScreen.java
@@ -0,0 +1,127 @@
+package com.shivam.hul_course_selector;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class SecondScreen extends Activity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.second);
+
+        Intent activitythat = getIntent();
+        int c=0;
+        String h ="",w="";
+        String prevac = activitythat.getExtras().getString("callAc");
+        for(int i=0;i<prevac.length();i++)
+        {
+
+            if(prevac.charAt(i)==' ')
+            {
+                c=1;
+            }
+
+            if(c==0)
+            {
+                h = h + prevac.charAt(i);
+            }
+            else
+            {
+                w = w + prevac.charAt(i);
+            }
+        }
+
+        TextView x = findViewById(R.id.ter);
+        TextView y = findViewById(R.id.ter2);
+        TextView z = findViewById(R.id.ter3);
+        TextView a = findViewById(R.id.ter4);
+        TextView b = findViewById(R.id.ter5);
+        x.setText(h);
+        if(h.equals("HUL212"))
+        {
+            String t = "Micro  versus  macroeconomics.  Theory  of  consumer  behavior  and  demand.  Consumer  preferences.  Indifference  curve.  Consumer  equilibrium. Demand function. Income and substitution effects. The Slutsky equation. Market demand. Elasticities. Average and marginal revenue. Revealed preference theory of firm. Production functions. Law of variable proportions. Laws of return to scale. Isoquants. Input substitution. Equilibrium of the firm. Expansion path. Cost function. Theory  of  costs.  Short  Run  and  Long  run  costs.  Shape  of  LAC.  Economies and diseconomies of scale. Market equilibrium under perfect competition. Equilibrium under alternative forms of market. Monopoly: pure  and  discriminating.  Monopolistic  competition.  Oligopoly.";
+            z.setText("Course Details: "+t);
+        }
+        else if(h.equals("HUL213"))
+        {
+            String t = "Major economic problems. National income accounting. Expenditure and income approaches to GNP. Measuring inflation and unemployment. Determination  of  the  equilibrium  level  of  income.  Consumption  function.  Investment  demand.  Aggregate  demand  and  equilibrium  output. The multiplier process. Government sector. Fiscal policy. Tax receipts  and  Transfer  payments.  Foreign  spending.  Money,  interest  and income. Functions of money. Definition of money. Reserve Bank of India and Commercial Banks. Creation of money. The instruments of monetary control. The demand for money. Investment expenditure and rate of interest. The IS curve. Money market and the LM curve. Liquidity trap. The IS-LM model. Derivation of the aggregate demand curve. Monetary and fiscal polices. Keynesian versus monetarist views. The aggregate supply function: Keynesian and classical. Inflation and unemployment. Stagflation. The Phillips curve. The long-run Phillips curve. Inflation expectations. The rational expectations.";
+            z.setText("Course Details: "+t);
+        }
+        else if(h.equals("HUL232"))
+        {
+            String t = "Students would be introduced to the conditions, beginning in 19th century colonial rule in India, which led to the emergent Indian middle-class intelligentsia to experiment with European forms of literature but striving for an alternative expression. Indian languages became the medium through which writers sought to address issues of identity, tradition, modernity, gender, the rural and the urban, the private and the public. The course will study the various experiments in narration, language, characterization and style undertaken by authors to shape these themes.";
+            z.setText("Course Details: "+t);
+        }
+        else if(h.equals("HUL239"))
+        {
+            String t = "The course involves a detailed study of 3-4 texts corresponding to the distinct phases of literary activity in the genre: the early period of the 1940s and 50s in which writers like Mulk Raj Anand, Raja Rao and R.K. Narayan made their presence felt, before Salman Rushdie, and more quietly, Amitav Ghosh and Vikram Seth, erupted into the scene in the 1980s, spawning a generation of writers attaining international acclaim - Arundhati Roy, Aravind Adiga, Kiran Desai, and many others. Some of the questions that will be addressed are: Who constitutes the main audience for this writing, and (how) does the writing cater to it? How does one position the expatriate Indian writer both residing and publishing abroad? How does English become an Indian language? Is there a thematic congruence in the novels that fall under this category, and does it differ from the thematic concerns of novels written in other Indian languages? Students will be encouraged to read a novel in at least one other Indian language in order to allow them to pose these questions in a more pointed manner.";
+            z.setText("Course Details: "+t);
+        }
+
+        if(w.equals(" Based on Professor Grading"))
+        {
+            b.setText("Prof Grading: Good");
+            if(h.equals("HUL212"))
+            {
+                String s = "Saptarshi Mukherjee";
+                y.setText(s);
+                a.setText("WorkLoad: Fighter");
+            }
+            else if(h.equals("HUL213"))
+            {
+                String s = "Sourabh Bikas Paul";
+                y.setText(s);
+                a.setText("WorkLoad: Fighter");
+            }
+            else if(h.equals("HUL232"))
+            {
+                String s = "Simona Sawhney";
+                y.setText(s);
+                a.setText("WorkLoad: chill");
+            }
+            else if(h.equals("HUL239"))
+            {
+                String s = "Angelia Multani";
+                y.setText(s);
+                a.setText("WorkLoad: chill");
+            }
+
+        }
+        else{
+
+            a.setText("WorkLoad : chill");
+            if(h.equals("HUL212"))
+            {
+                String s = "Saptarshi Mukherjee";
+                y.setText(s);
+                b.setText("Prof Grading: Good");
+            }
+            else if(h.equals("HUL213"))
+            {
+                String s = "V.Upadhyay";
+                y.setText(s);
+                b.setText("Prof Grading: Very Good");
+            }
+            else if(h.equals("HUL232"))
+            {
+                String s = "Simona Sawhney";
+                y.setText(s);
+                b.setText("Prof Grading: Good");
+            }
+            else if(h.equals("HUL239"))
+            {
+                String s = "Angelia Multani";
+                y.setText(s);
+                b.setText("Prof Grading: Good");
+            }
+
+        }
+
+
+    }
+}
